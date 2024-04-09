2b72dfb6f609aa98bcc41ee3d014256790dbf7ce
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-04-09 15:17:02 +0700

    split files

--- infra/main.tf
+++ infra/main.tf
@@ -10,25 +10,3 @@ terraform {
 provider "aws" {
   region = "ap-southeast-1"
 }
-
-resource "aws_s3_bucket" "frontend_bucket" {
-  bucket        = "tuannamnguyen-resume-frontend-bucket"
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_public_access_block" "fe_bucket_access_block" {
-  bucket                  = aws_s3_bucket.frontend_bucket.id
-
-  block_public_acls       = false
-  ignore_public_acls      = false
-  block_public_policy     = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_website_configuration" "fe_static_site_config" {
-  bucket = aws_s3_bucket.frontend_bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-}
