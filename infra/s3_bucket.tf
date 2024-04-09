b3f1299229b3aa4790b2b0f38ea139300caee3ce
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-04-09 16:02:26 +0700

    add bucket policy

--- infra/s3_bucket.tf
+++ infra/s3_bucket.tf
@@ -19,3 +19,24 @@ resource "aws_s3_bucket_website_configuration" "fe_static_site_config" {
     suffix = "index.html"
   }
 }
+
+
+resource "aws_s3_bucket_policy" "fe_bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "PublicReadGetObject",
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : [
+          "s3:GetObject"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::${aws_s3_bucket.frontend_bucket.bucket}/*"
+        ]
+      }
+    ]
+  })
+}
