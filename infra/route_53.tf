bc5c6a0dd395a658d95ea703a7219e62834fe93e
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-04-14 22:40:12 +0700

    add A records for cloudfront distribution

--- infra/route_53.tf
+++ infra/route_53.tf
@@ -7,18 +7,6 @@ resource "aws_route53_zone" "fe-hosted-zone" {
   force_destroy = true
 }
 
-resource "aws_route53_record" "fe-route53-record" {
-  zone_id = aws_route53_zone.fe-hosted-zone.zone_id
-  name    = local.domain_name
-  type    = "A"
-
-  alias {
-    zone_id                = aws_s3_bucket.frontend_bucket.hosted_zone_id
-    name                   = aws_s3_bucket_website_configuration.fe_static_site_config.website_domain
-    evaluate_target_health = "false"
-  }
-}
-
 resource "aws_route53_record" "records-for-acm" {
   for_each = {
     for dvo in aws_acm_certificate.fe-bucket-certificate.domain_validation_options : dvo.domain_name => {
@@ -35,3 +23,15 @@ resource "aws_route53_record" "records-for-acm" {
   type    = each.value.type
 }
 
+
+resource "aws_route53_record" "record-route-to-cloudfront" {
+  zone_id = aws_route53_zone.fe-hosted-zone.zone_id
+  name    = local.domain_name
+  type    = "A"
+
+  alias {
+    zone_id                = aws_cloudfront_distribution.fe-cloudfront-distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.fe-cloudfront-distribution.domain_name
+    evaluate_target_health = "false"
+  }
+}
