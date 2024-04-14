8bc8d19fcf11b0c521d0df33e5a0900dde10ad52
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-04-14 22:57:18 +0700

    add namecheap provider

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.44.0"
     }
+    namecheap = {
+      source  = "namecheap/namecheap"
+      version = "2.1.2"
+    }
   }
+
+
 
   backend "s3" {
     region = "ap-southeast-1"
