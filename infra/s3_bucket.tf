d8615b02b3b4f7dc400eff1c2dbb0f023a33f15e
tuannamnguyen <tuannamnguyen290602@gmail.com>
2024-04-15 14:05:01 +0700

    Revert "disable public access to s3 bucket & enable bucket versioning"
    This reverts commit 18428f31580f5c855d099a245fe980206bedc99d.

--- infra/s3_bucket.tf
+++ infra/s3_bucket.tf
@@ -7,11 +7,44 @@ resource "aws_s3_bucket" "frontend_bucket" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_versioning" "fe-bucket-versioning"   {
+resource "aws_s3_bucket_public_access_block" "fe_bucket_access_block" {
   bucket = aws_s3_bucket.frontend_bucket.id
-  versioning_configuration {
-    status = "Enabled"
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "fe_static_site_config" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
   }
+}
+
+
+resource "aws_s3_bucket_policy" "fe_bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "PublicReadGetObject",
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : [
+          "s3:GetObject"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::${aws_s3_bucket.frontend_bucket.bucket}/*"
+        ]
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.fe_bucket_access_block]
 }
 
 locals {
